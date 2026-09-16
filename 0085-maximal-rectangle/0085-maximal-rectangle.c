1d863b8c0d0074ba8e568a960afdc7e3c7bbdef1
aakshaya302007 <aakshayavengatesan@gmail.com>
2026-09-16 21:27:53 +0530

    Time: 3 ms (55.83%), Space: 12.6 MB (12.91%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.c
@@ -0,0 +1,58 @@
+int largestRectangleArea(int* heights, int n) {
+    int *stack = malloc((n + 1) * sizeof(int));
+    int top = -1;
+    int maxArea = 0;
+
+    for (int i = 0; i <= n; i++) {
+        int current = (i == n) ? 0 : heights[i];
+
+        while (top >= 0 && heights[stack[top]] > current) {
+            int height = heights[stack[top--]];
+
+            int width;
+
+            if (top == -1)
+                width = i;
+            else
+                width = i - stack[top] - 1;
+
+            int area = height * width;
+
+            if (area > maxArea)
+                maxArea = area;
+        }
+
+        stack[++top] = i;
+    }
+
+    free(stack);
+    return maxArea;
+}
+
+int maximalRectangle(char** matrix, int matrixSize, int* matrixColSize) {
+    if (matrixSize == 0)
+        return 0;
+
+    int cols = matrixColSize[0];
+    int *heights = calloc(cols, sizeof(int));
+    int maxArea = 0;
+
+    for (int i = 0; i < matrixSize; i++) {
+
+        for (int j = 0; j < cols; j++) {
+            if (matrix[i][j] == '1')
+                heights[j]++;
+            else
+                heights[j] = 0;
+        }
+
+        int area = largestRectangleArea(heights, cols);
+
+        if (area > maxArea)
+            maxArea = area;
+    }
+
+    free(heights);
+
+    return maxArea;
+}
